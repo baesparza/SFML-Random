e7080bf2afcc9bc49527c9e692931f7b4b146bf4
baesparza <bruno.be81@gmail.com>
2018-02-16 10:17:39 -0500

    feat(math): add formulas

--- 02-Lorenz_Attractor/src/main.cpp
+++ 02-Lorenz_Attractor/src/main.cpp
@@ -1,29 +1,59 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+
+
+// variables
+float x = 0.01;
+float y = 0.01;
+float z = 0.01;
+
+// constant
+float a = 10;
+float b = 28;
+float c = 8.f / 3.f;
+
+// window
+const int WIDTH = 600, HEIGHT = 500;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600),
-		"Hello SFML", sf::Style::Default);
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Lorenz Attractor");
 
-	sf::Font font;
-	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+	window.setFramerateLimit(60);
 
-	sf::Text text;
-	text.setFont(font);
-	text.setPosition(200, 200);
-	text.setString("Bruno Esparza");
+	CircleShape circle;
+	circle.setRadius(2);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+		Event e;
+		while (window.pollEvent(e))
+			if (e.type == Event::Closed)
 				window.close();
-		}
-		window.clear();
-		window.draw(text);
+
+		// get data 
+		float dt = 0.01;
+		float dx = (a * (y - x)) * dt;
+		float dy = (x * (b - z) - y) * dt;
+		float dz = (x * y - c * z) * dt;
+
+		x += dx;
+		y += dy;
+		z += dz;
+
+		circle.setPosition(WIDTH / 2 + x, HEIGHT / 2 + y);
+		window.draw(circle);
+
+		///drawing///
 		window.display();
+
+
+		std::cout << x << ", " << y << ", " << z << "\n";
 	}
+
+
+
 	return 0;
 }
