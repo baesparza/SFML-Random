0e2ec288459305c3df12cdb49c9cbba47f670ea2
baesparza <bruno.be81@gmail.com>
2018-02-18 21:08:34 -0500

    feat(*): first commit

---
+++ 03-Travelling_Salesman_Problem/src/Main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+// declarations
+void setup(RenderWindow &);
+void draw(RenderWindow &);
+
+
+// window
+const int SIZE = 800;
+
+int main()
+{
+	RenderWindow app(VideoMode(SIZE, SIZE), "Traversing Salesman Problem");
+	app.setFramerateLimit(60);
+
+	setup(app);
+
+	while (app.isOpen())
+	{
+		Event e;
+		while (app.pollEvent(e))
+			if (e.type == Event::Closed)
+				app.close();
+
+		draw(app);
+
+		app.display();
+	}
+	return 0;
+}
---
+++ 03-Travelling_Salesman_Problem/src/Source.cpp
@@ -0,0 +1,16 @@
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+// window
+extern const int SIZE = 800;
+
+void setup(RenderWindow & app)
+{
+
+}
+
+void draw(RenderWindow & app)
+{
+
+}
