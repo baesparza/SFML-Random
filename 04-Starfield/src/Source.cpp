c99bb622718816b1bdd42b7adfd04a7b2f5020b2
baesparza <bruno.be81@gmail.com>
2018-02-20 11:40:33 -0500

    feat(center): center origin

--- 04-Starfield/src/Source.cpp
+++ 04-Starfield/src/Source.cpp
@@ -7,12 +7,12 @@ using namespace sf;
 
 CircleShape circle;
 
+int SIZE;
+
 // take one value in one range, and return its value in the second range
 float map(float num, float start1, float stop1, float start2, float stop2)
 {
-	float res = (num - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-	// std::cout << res << " = " << (num - start1) << " / " << (stop1 - start1) << " * " << (stop2 - start2) << " + " << start2 << std::endl;
-	return	res;
+	return (num - start1) / (stop1 - start1) * (stop2 - start2) + start2;
 }
 
 struct Star
@@ -21,9 +21,9 @@ struct Star
 
 	void star(RenderWindow & app)
 	{
-		x = rand() % app.getSize().x;
-		y = rand() % app.getSize().y;
-		z = app.getSize().x;
+		x = rand() % SIZE - (SIZE / 2);
+		y = rand() % SIZE - (SIZE / 2);
+		z = rand() % SIZE;
 	}
 
 	void update(RenderWindow & app)
@@ -35,10 +35,10 @@ struct Star
 
 	void draw(RenderWindow & app)
 	{
-		float sx = map(x / z, 0, 1, 0, app.getSize().x);
-		float sy = map(y / z, 0, 1, 0, app.getSize().y);
+		float sx = map(x / z, 0, 1, 0, SIZE);
+		float sy = map(y / z, 0, 1, 0, SIZE);
 		//circle.setRadius(2);
-		circle.setPosition(sx, sy);
+		circle.setPosition(SIZE/2 + sx, SIZE / 2 + sy);
 		app.draw(circle);
 	}
 };
@@ -47,6 +47,8 @@ std::array<Star, 500> stars;
 
 void setup(RenderWindow & app)
 {
+	SIZE = app.getSize().x;
+
 	srand(time(0));
 
 	circle.setRadius(2);
