d3e88b79159ca60eb64df7537a76253eed91c57f
baesparza <bruno.be81@gmail.com>
2018-02-22 21:01:07 -0500

    feat(branch): draw branch

--- 05-Fractal_Free/src/Main.cpp
+++ 05-Fractal_Free/src/Main.cpp
@@ -1,15 +1,21 @@
-#include <SFML/Graphics.hpp>
 #include <cmath>
+#include <iostream>
+#include <SFML/Graphics.hpp>
 
 // define pi
 const double PI = std::acos(-1);
+
+// options
+const int angle_increment = 45;
+const float magn_dec = 0.6;
+
 
 struct Tree
 {
 	sf::Vector2f start, end;
 	float angle;
 
-	// Tree *left, *right;
+	Tree *left, *right;
 
 	Tree(sf::Vector2f strt, int alpha, int magnitude) :
 		start(strt)
@@ -20,6 +26,15 @@ struct Tree
 		float end_x = magnitude * std::cos(angle);
 		float end_y = magnitude * std::sin(angle);
 		end = {start.x - end_x, start.y - end_y};
+
+		// initialize child
+		if (magnitude > 2)
+		{
+			right = new Tree(end, alpha - angle_increment, magnitude * magn_dec);
+			left = new Tree(end, alpha + angle_increment, magnitude * magn_dec);
+			return;
+		}
+		left = right = nullptr;
 	}
 
 	void show(sf::RenderWindow & app)
@@ -27,6 +42,10 @@ struct Tree
 		// line obj
 		sf::Vertex line[2] = {start, end};
 		app.draw(line, 2, sf::Lines);
+
+		// print child recursively
+		if (right) right->show(app);
+		if (left) left->show(app);
 	}
 };
 
@@ -36,10 +55,9 @@ int main()
 
 	// main tree
 	Tree * root = new Tree(
-		//	sf::Vector2f{app.getSize().x / 2, app.getSize().y}, // beging center bottom
-	{400, 800},
+		sf::Vector2f{float(app.getSize().x / 2), float(app.getSize().y)}, // beging center bottom
 		90,
-		400
+		app.getSize().y * 0.35
 	);
 
 
