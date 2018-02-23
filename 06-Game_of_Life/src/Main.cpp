cae5ab14ddae0917d7e4cd286bb7a0ac04771d9c
baesparza <bruno.be81@gmail.com>
2018-02-23 11:10:30 -0500

    feat(game): add game logic

--- 06-Game_of_Life/src/Main.cpp
+++ 06-Game_of_Life/src/Main.cpp
@@ -5,14 +5,49 @@
 const int TS = 10;
 const int COL = 90, ROW = 90;
 
-struct Square
+bool grid[ROW][COL];
+bool t_grid[ROW + 2][COL + 2];
+
+void updateGrid()
 {
-	bool live;
-	Square()
-	{
-		live = rand() % 2 - 1;
-	}
-} grid[ROW][COL];
+	// clone main grid
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < COL; j++)
+			t_grid[i + 1][j + 1] = grid[i][j];
+
+	// verify every grid if alive
+	for (int i = 1; i <= ROW; i++)
+		for (int j = 1; j <= COL; j++)
+		{
+			int n = 0;
+
+			// counter near alive
+			if (t_grid[i - 1][j - 1]) n++; // top left
+			if (t_grid[i - 1][j]) n++; // top
+			if (t_grid[i - 1][j + 1]) n++; // top right
+			if (t_grid[i][j + 1]) n++; // right
+			if (t_grid[i + 1][j + 1]) n++; // bottom right
+			if (t_grid[i + 1][j]) n++; // bottom
+			if (t_grid[i + 1][j - 1]) n++; // bottom left
+			if (t_grid[i][j - 1]) n++; // left
+
+			// cell die
+			if (t_grid[i][j]) // cell alive
+				t_grid[i][j] = (n == 2 || n == 3) ? true : false;
+
+
+
+			// cell gets alive
+			if (!t_grid[i][j]) // cell dead
+				if (n == 3)
+					t_grid[i][j] = true;
+		}
+
+	// actualizar main grid
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < COL; j++)
+			grid[i][j] = t_grid[i + 1][j + 1];
+}
 
 int main()
 {
@@ -23,7 +58,13 @@ int main()
 	// rectangle
 	sf::RectangleShape rectangle(sf::Vector2f(TS, TS));
 	rectangle.setOutlineColor(sf::Color::Black);
-	rectangle.setOutlineThickness(2);
+	rectangle.setOutlineThickness(1);
+
+	// initialize grid
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < COL; j++)
+			grid[i][j] = (rand() % 10 == 2) ? true : false;
+
 	while (app.isOpen())
 	{
 		sf::Event e;
@@ -41,9 +82,12 @@ int main()
 			for (int j = 0; j < COL; j++)
 			{
 				rectangle.setPosition(j * TS, i * TS);
-				rectangle.setFillColor((grid[i][j].live) ? sf::Color::Black : sf::Color::White); // live -> brack, else white
+				rectangle.setFillColor((grid[i][j]) ? sf::Color::Black : sf::Color::White); // live -> brack, else white
 				app.draw(rectangle);
 			}
+
+		// update grid
+		updateGrid();
 
 		app.display();
 	}
