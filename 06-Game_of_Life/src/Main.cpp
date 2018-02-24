bce9fb5fc00cca7bc92f4e36c2ff0b59c2e27a71
baesparza <bruno.be81@gmail.com>
2018-02-24 00:17:22 -0500

    feat(algo): rework update

--- 06-Game_of_Life/src/Main.cpp
+++ 06-Game_of_Life/src/Main.cpp
@@ -6,47 +6,40 @@ const int TS = 10;
 const int COL = 90, ROW = 90;
 
 bool grid[ROW][COL];
-bool t_grid[ROW + 2][COL + 2];
+bool t_grid[ROW][COL];
 
 void updateGrid()
 {
 	// clone main grid
 	for (int i = 0; i < ROW; i++)
 		for (int j = 0; j < COL; j++)
-			t_grid[i + 1][j + 1] = grid[i][j];
+			t_grid[i][j] = grid[i][j];
 
 	// verify every grid if alive
-	for (int i = 1; i <= ROW; i++)
-		for (int j = 1; j <= COL; j++)
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < COL; j++)
 		{
 			int n = 0;
 
 			// counter near alive
-			if (t_grid[i - 1][j - 1]) n++; // top left
-			if (t_grid[i - 1][j]) n++; // top
-			if (t_grid[i - 1][j + 1]) n++; // top right
-			if (t_grid[i][j + 1]) n++; // right
-			if (t_grid[i + 1][j + 1]) n++; // bottom right
-			if (t_grid[i + 1][j]) n++; // bottom
-			if (t_grid[i + 1][j - 1]) n++; // bottom left
-			if (t_grid[i][j - 1]) n++; // left
+			if (t_grid[(i - 1 + ROW) % ROW][(j - 1 + COL) % COL]) n++; // top left
+			if (t_grid[(i - 1 + ROW) % ROW][(j + 1 + COL) % COL]) n++; // top right
+			if (t_grid[(i + 1 + ROW) % ROW][(j + 1 + COL) % COL]) n++; // bottom right
+			if (t_grid[(i + 1 + ROW) % ROW][(j - 1 + COL) % COL]) n++; // bottom left
+			if (t_grid[(i - 1 + ROW) % ROW][j]) n++; // top
+			if (t_grid[(i + 1 + ROW) % ROW][j]) n++; // bottom
+			if (t_grid[i][(j + 1 + COL) % COL]) n++; // right
+			if (t_grid[i][(j - 1 + COL) % COL]) n++; // left
 
 			// cell die
 			if (t_grid[i][j]) // cell alive
-				t_grid[i][j] = (n == 2 || n == 3) ? true : false;
-
-
+				grid[i][j] = (n == 2 || n == 3) ? true : false;
 
 			// cell gets alive
 			if (!t_grid[i][j]) // cell dead
 				if (n == 3)
-					t_grid[i][j] = true;
+					grid[i][j] = true;
 		}
-
-	// actualizar main grid
-	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < COL; j++)
-			grid[i][j] = t_grid[i + 1][j + 1];
 }
 
 int main()
@@ -54,6 +47,7 @@ int main()
 	srand(time(0));
 
 	sf::RenderWindow app(sf::VideoMode(TS * COL, TS * ROW), "Game of life");
+	app.setFramerateLimit(60);
 
 	// rectangle
 	sf::RectangleShape rectangle(sf::Vector2f(TS, TS));
@@ -64,15 +58,20 @@ int main()
 	for (int i = 0; i < ROW; i++)
 		for (int j = 0; j < COL; j++)
 			grid[i][j] = (rand() % 10 == 2) ? true : false;
+	//	grid[10][10] = grid[11][11] = grid[12][11] = grid[10][12] = grid[11][12] = true; // test
 
 	while (app.isOpen())
 	{
+
 		sf::Event e;
 		while (app.pollEvent(e))
 		{
 			if (e.type == sf::Event::Closed)
 				app.close();
 		}
+
+		// update grid
+		updateGrid();
 
 		/////draw/////
 		app.clear(sf::Color::White);
@@ -86,8 +85,6 @@ int main()
 				app.draw(rectangle);
 			}
 
-		// update grid
-		updateGrid();
 
 		app.display();
 	}
