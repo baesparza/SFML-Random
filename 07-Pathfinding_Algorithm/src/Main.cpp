433c61386ed0db3f6ca7663a1c8e5678ce11a6d6
baesparza <bruno.be81@gmail.com>
2018-02-27 22:04:25 -0500

    feat(A*): first commit

--- 07-Pathfinding_Algorithm/src/Main.cpp
+++ 07-Pathfinding_Algorithm/src/Main.cpp
@@ -1,53 +1,89 @@
+#include <iostream>
+#include <array>
+#include <vector>
 #include <SFML/Graphics.hpp>
 
-const int COLS = 90;
-const int ROWS = 90;
-const int TS = 10;
+const int COLS = 10;
+const int ROWS = 10;
+const int TS = 40;
 
-int grid[ROWS][COLS];
+sf::RectangleShape rectangle(sf::Vector2f(TS - 1, TS - 1));
 
-sf::Color getColor(int n)
+struct Cell
 {
-	if (n == 0) return sf::Color::White; // empty
-	if (n == 1) return sf::Color::Black; // occupated
-	if (n == 2) return sf::Color::Cyan; // path
-	if (n == 3) return sf::Color::Blue; // head
-}
+	int x, y; // position
+	int f, g, h;
+
+	void setPosition(int i, int j)
+	{
+		x = j;
+		y = i;
+	}
+
+	void show(sf::RenderWindow & app, sf::Color color)
+	{
+		rectangle.setPosition(x*TS, y*TS);
+		rectangle.setFillColor(color);
+		app.draw(rectangle);
+	}
+
+};
+
 
 int main()
 {
 	srand(time(0));
 	sf::RenderWindow app(sf::VideoMode(COLS * TS, ROWS * TS), "Pathfinding Algorithm");
-	app.setFramerateLimit(10);
+	//	app.setFramerateLimit(60);
 
 	// rectangle
-	sf::RectangleShape rectangle(sf::Vector2f(TS, TS));
 	rectangle.setOutlineColor(sf::Color::Black);
 	rectangle.setOutlineThickness(1);
 
-	// initialize grid
+	// initialize with positions
+	std::array<std::array<Cell, COLS>, ROWS> grid;
 	for (int i = 0; i < ROWS; i++)
-		for (int j = 0; j < ROWS; j++)
-			grid[i][j] = rand() % 4; // (rand() % 5 == 1) ? 1 : 0;
+		for (int j = 0; j < COLS; j++)
+			grid[i][j].setPosition(i, j);
+
+	/// start and end used in algorithm
+	Cell start = grid[0][0];
+	Cell end = grid[ROWS - 1][COLS - 1];
+
+	/// The set of nodes already evaluated
+	std::vector<Cell> closedSet;
+
+	/// The set of currently discovered nodes that are not evaluated yet.
+	/// Initially, only the start node is known.
+	std::vector<Cell> openSet{start};
+
 
 	while (app.isOpen())
 	{
+		// window open
 		sf::Event e;
 		while (app.pollEvent(e))
 			if (e.type == sf::Event::Closed)
 				app.close();
-		/////draw/////
+
+		/// algorithm no solution
+		if (openSet.size() == 0) continue;
+
 		app.clear();
 
+		/////draw/////
+		// draw all grid
 		for (int i = 0; i < ROWS; i++)
-			for (int j = 0; j < ROWS; j++)
-			{
-				rectangle.setPosition(j*TS, i*TS);
-				rectangle.setFillColor(getColor(grid[i][j]));
-				app.draw(rectangle);
-			}
+			for (int j = 0; j < COLS; j++)
+				grid[i][j].show(app, sf::Color::White);
+
+		// draw openset and closet
+		for (Cell c : openSet) c.show(app, sf::Color::Cyan);
+		for (Cell c : closedSet) c.show(app, sf::Color::Magenta);
+
 
 		app.display();
+
 	}
 
 	return 0;
