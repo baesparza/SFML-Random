063ef5d38a9f7caa57542a37fa6a9bdebcce664c
baesparza <bruno.be81@gmail.com>
2018-02-28 11:35:02 -0500

    feat(remove): add remove from vector function

--- 07-Pathfinding_Algorithm/src/Main.cpp
+++ 07-Pathfinding_Algorithm/src/Main.cpp
@@ -3,21 +3,21 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
-const int COLS = 10;
-const int ROWS = 10;
-const int TS = 40;
+const int COLS = 5;
+const int ROWS = 6;
+const int TS = 60;
 
 sf::RectangleShape rectangle(sf::Vector2f(TS - 1, TS - 1));
 
-struct Cell
+class Cell
 {
+public:
 	int x, y; // position
 	int f, g, h;
-
-	void setPosition(int i, int j)
+public:
+	Cell(int i, int j) : x(j), y(i)
 	{
-		x = j;
-		y = i;
+		f = g = h = 0;
 	}
 
 	void show(sf::RenderWindow & app, sf::Color color)
@@ -27,36 +27,53 @@ struct Cell
 		app.draw(rectangle);
 	}
 
+	bool operator==(const Cell & temp)
+	{
+		return x == temp.x && y == temp.y;
+	}
+
+	~Cell()
+	{
+		std::cout << "destroyed" << "\n";
+	}
 };
+
+void removeFromVector(std::vector<Cell *> & vector, Cell * element)
+{
+	for (int i = vector.size() - 1; i >= 0; i--)
+		if (element == vector[i])
+		{
+			vector.erase(vector.begin() + i);
+			return;
+		}
+}
 
 
 int main()
 {
-	srand(time(0));
+	//	srand(time(0));
 	sf::RenderWindow app(sf::VideoMode(COLS * TS, ROWS * TS), "Pathfinding Algorithm");
-	//	app.setFramerateLimit(60);
 
 	// rectangle
 	rectangle.setOutlineColor(sf::Color::Black);
 	rectangle.setOutlineThickness(1);
 
 	// initialize with positions
-	std::array<std::array<Cell, COLS>, ROWS> grid;
-	for (int i = 0; i < ROWS; i++)
-		for (int j = 0; j < COLS; j++)
-			grid[i][j].setPosition(i, j);
+	std::array<std::array<Cell *, COLS>, ROWS> grid;
+	for (int i = 0; i < grid.size(); i++)
+		for (int j = 0; j < grid[i].size(); j++)
+			grid[i][j] = new Cell(i, j);
 
 	/// start and end used in algorithm
-	Cell start = grid[0][0];
-	Cell end = grid[ROWS - 1][COLS - 1];
+	Cell * start = grid[0][0];
+	Cell * end = grid[ROWS - 1][COLS - 1];
 
 	/// The set of nodes already evaluated
-	std::vector<Cell> closedSet;
+	std::vector<Cell *> closedSet;
 
 	/// The set of currently discovered nodes that are not evaluated yet.
 	/// Initially, only the start node is known.
-	std::vector<Cell> openSet{start};
-
+	std::vector<Cell *> openSet{start};
 
 	while (app.isOpen())
 	{
@@ -66,24 +83,35 @@ int main()
 			if (e.type == sf::Event::Closed)
 				app.close();
 
+
 		/// algorithm no solution
 		if (openSet.size() == 0) continue;
 
-		app.clear();
+		/// the node in openSet having the lowest fScore[] value
+		Cell * current = openSet[0];
+		for (int i = 0; i < openSet.size(); i++)
+			if (openSet[i]->f < current->f)
+				current = openSet[i];
 
-		/////draw/////
+		/// check if we are done
+		if (current == end)
+			std::cout << "DONE" << std::endl;
+
+		//removeFromVector(openSet, current);
+		closedSet.push_back(current);
+
+		//////////draw//////////
+		app.clear();
 		// draw all grid
-		for (int i = 0; i < ROWS; i++)
-			for (int j = 0; j < COLS; j++)
-				grid[i][j].show(app, sf::Color::White);
+		for (int i = 0; i < grid.size(); i++)
+			for (int j = 0; j < grid[i].size(); j++)
+				grid[i][j]->show(app, sf::Color::White);
 
 		// draw openset and closet
-		for (Cell c : openSet) c.show(app, sf::Color::Cyan);
-		for (Cell c : closedSet) c.show(app, sf::Color::Magenta);
-
+		for (Cell * c : closedSet) c->show(app, sf::Color::Red);
+		for (Cell * c : openSet) c->show(app, sf::Color::Green);
 
 		app.display();
-
 	}
 
 	return 0;
